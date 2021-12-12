37e424a8a3fd2b01b1aeceef15c42aa11c8a6593
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2021-12-12 23:17:53 +0100

    Fix build script for JDK 17

--- greenhouse/build-macos.sh
+++ greenhouse/build-macos.sh
@@ -1,9 +1,8 @@
-JAVA_FILES=($(find . -name \*.java))
 GIF_FILES=($(cd src; find . -name \*.gif))
-javac -d bin $FILES
+javac -d bin --module-source-path greenhouse=src -m greenhouse
 for F in $GIF_FILES
 do
-  cp src/$F bin/$F
+  cp src/$F bin/greenhouse/$F
 done
 jpackage -p bin -m greenhouse/greenhouse.Main --name Greenhouse --type app-image
 codesign --remove-signature Greenhouse.app
