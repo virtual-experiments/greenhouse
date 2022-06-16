7feb367efaad769136166cf1ca0c63cec448523d
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-16 10:51:38 +0200

    Sign MacOS release (part 2)

--- greenhouse/build-macos.sh
+++ greenhouse/build-macos.sh
@@ -4,7 +4,8 @@ for F in $GIF_FILES
 do
   cp src/$F bin/greenhouse/$F
 done
-jpackage -p bin -m greenhouse/greenhouse.Main --name Greenhouse --type app-image
-codesign --remove-signature Greenhouse.app
-codesign --remove-signature Greenhouse.app/Contents/runtime
+jpackage -p bin -m greenhouse/greenhouse.Main --name Greenhouse --type app-image --mac-sign
+zip -r Greenhouse-macos-unstapled.zip Greenhouse.app
+xcrun notarytool submit Greenhouse-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
+xcrun stapler staple Greenhouse.app
 zip -r Greenhouse-macos.zip Greenhouse.app
