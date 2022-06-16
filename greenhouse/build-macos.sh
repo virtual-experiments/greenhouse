5ae32f7231b5b2d9d5ab09625498ef1313323c90
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-16 11:07:21 +0200

    Use ditto instead of zip to create archive
    Hopefully fixes the notarization failure

--- greenhouse/build-macos.sh
+++ greenhouse/build-macos.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 GIF_FILES=($(cd src; find . -name \*.gif))
 javac -d bin --module-source-path greenhouse=src -m greenhouse
 for F in $GIF_FILES
@@ -5,7 +9,8 @@ do
   cp src/$F bin/greenhouse/$F
 done
 jpackage -p bin -m greenhouse/greenhouse.ui.ExperimentView --name Greenhouse --type app-image --mac-sign
-zip -r Greenhouse-macos-unstapled.zip Greenhouse.app
+ditto -c -k --keepParent --sequesterRsrc Greenhouse.app Greenhouse-macos-unstapled.zip
 xcrun notarytool submit Greenhouse-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
+rm Greenhouse-macos-unstapled.zip
 xcrun stapler staple Greenhouse.app
-zip -r Greenhouse-macos.zip Greenhouse.app
+ditto -c -k --keepParent --sequesterRsrc Greenhouse.app Greenhouse-macos.zip
